8aeb1f78ba03ad18448f5a5161ca2a2ad81d2a64
sagarborse0525 <92211637+sagarborse0525@users.noreply.github.com>
2023-01-07 04:19:56 +0000

    update

--- rds.tf
+++ rds.tf
@@ -1,5 +1,5 @@
 resource "aws_db_instance" "default" {
-  allocated_storage    = 10
+  allocated_storage    = 15
   db_name              = "mydb"
   engine               = "mysql"
   engine_version       = "5.7"
