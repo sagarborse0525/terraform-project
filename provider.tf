02d9cb4b055d82a3f9b8b8cdf4598f0a4124c8bf
Sagar Borse <2415027@TCS.com>
2023-08-20 11:32:24 +0530

    Updated provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,8 @@
 provider "aws" {
-  # Configuration options
-  region = "ap-south-1"
+    region = "us-east-1"
+
+    assume_role {
+        role_arn = "arn:aws:iam::108040610828:role/terraform-role"
+    }
   
 }
