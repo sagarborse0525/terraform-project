f7a6da966451b879ce82151319cbafb6976a0a4b
Sagar Borse <2415027@TCS.com>
2023-08-27 17:52:50 +0530

    Updated ECS Service

--- modules/ECS/service/service.tf
+++ modules/ECS/service/service.tf
@@ -2,17 +2,88 @@
 # AWS ECS SERVICE
 #------------------------------------------------------------------------------
 
-resource "aws_ecs_service" "mongo" {
-  name            = "${var.ecs_service_name}-service"
-  cluster         = var.ecs_cluster_arn
-  deployment_maximum_percent = var.deployment_maximum_percent
+resource "aws_ecs_service" "main" {
+  count                              = var.create ? 1 : 0
+  name                               = "${var.ecs_service_name}-service"
+  cluster                            = var.ecs_cluster_arn
+  task_definition                    = var.task_definition_arn
+  deployment_maximum_percent         = var.deployment_maximum_percent
   deployment_minimum_healthy_percent = var.deployment_minimum_healthy_percent
-  desired_count = var.desired_count
-  enable_execute_command = var.enable_execute_command
-  enable_ecs_managed_tags = var.enable_ecs_managed_tags
-  health_check_grace_period_seconds = var.health_check_grace_period_seconds
-  launch_type = var.launch_type 
-  force_new_deployment = force_new_deployment
+  desired_count                      = var.desired_count
+  enable_execute_command             = var.enable_execute_command
+  enable_ecs_managed_tags            = var.enable_ecs_managed_tags
+  health_check_grace_period_seconds  = var.health_check_grace_period_seconds
+  launch_type                        = var.launch_type
+  force_new_deployment               = var.force_new_deployment
+  platform_version                   = var.launch_type == "FARGATE" ? var.platform_version : null
+  scheduling_strategy                = var.launch_type == "FARGATE" ? "REPLICA" : var.scheduling_strategy
+  propagate_tags                     = var.propagate_tags
 
-  
+  dynamic "capacity_provider_strategy" {
+    for_each = var.capacity_provider_strategiesy
+    content {
+      base              = lookup(capacity_provider_strategy.value, "base", null) //Optional
+      weight            = capacity_provider_strategy.value.weight                //Required
+      capacity_provider = capacity_provider_strategy.value.capacity_provider
+
+    }
+
+  }
+
+  dynamic "service_registries" {
+    for_each = var.service_registries
+    content {
+      registry_arn   = service_registries.value.registry_arn
+      port           = lookup(service_registries.value, "port", null)
+      container_port = lookup(service_registries.value, "container_port", null)
+      container_name = lookup(service_registries.value, "container_name", null)
+    }
+  }
+
+  dynamic "ordered_placement_strategy" {
+    for_each = var.ordered_placement_strategy
+    content {
+      type  = ordered_placement_strategy.value.type
+      field = lookup(ordered_placement_strategy.value, "field", null)
+    }
+
+  }
+
+  dynamic "placement_constraints" {
+    for_each = var.placement_constraints
+    content {
+      type       = placement_constraints.value.type
+      expression = lookup(placement_constraints.value, "placement_constraints", null)
+    }
+  }
+
+  dynamic "load_balancer" {
+    for_each = var.load_balancer
+    content {
+      elb_name         = load_balancer.value.elb_name
+      target_group_arn = load_balancer.value.target_group_arn
+      container_name   = load_balancer.value.container_name
+      container_port   = load_balancer.value.container_port
+    }
+  }
+
+  dynamic "deployment_controller" {
+    for_each = var.deployment_controller
+    content {
+      type = lookup(deployment_controller.value, "type", null)
+    }
+  }
+
+  network_configuration {
+    subnets          = var.assign_public_ip ? var.public_subnets : var.private_subnets
+    security_groups  = var.security_group_ids
+    assign_public_ip = var.assign_public_ip
+  }
+
+
+
+
+
+
+
 }
---
+++ modules/ECS/service/variables.tf
@@ -0,0 +1,167 @@
+#variable for ECS_service
+
+variable "create" {
+  description = "Boolean value for creation ecs service"
+  type        = bool
+  default     = true
+}
+
+variable "ecs_service_name" {
+  description = "The name of the service."
+  type        = string
+}
+
+variable "ecs_cluster_name" {
+  description = "(Optional) Name of the ECS cluster. Required only if autoscaling is enabled"
+  type        = string
+  default     = null
+}
+
+variable "task_definition_arn" {
+  description = "(Required) The full ARN of the task definition that you want to run in your service."
+}
+
+variable "deployment_maximum_percent" {
+  description = "(Optional) The upper limit (as a percentage of the service's desiredCount) of the number of running tasks that can be running in a service during a deployment."
+  type        = number
+  default     = 200
+}
+
+variable "deployment_minimum_healthy_percent" {
+  description = "(Optional) The lower limit (as a percentage of the service's desiredCount) of the number of running tasks that must remain running and healthy in a service during a deployment."
+  type        = number
+  default     = 100
+}
+
+variable "desired_count" {
+  description = "(Optional) The number of instances of the task definition to place and keep running. Defaults to 0."
+  type        = number
+  default     = 1
+}
+
+variable "enable_execute_command" {
+  description = "(Optional) Specifies whether to enable Amazon ECS Exec for the tasks within the service."
+  type        = bool
+  default     = false
+}
+
+variable "enable_ecs_managed_tags" {
+  description = "(Optional) Specifies whether to enable Amazon ECS managed tags for the tasks within the service."
+  type        = bool
+  default     = false
+}
+
+variable "health_check_grace_period_seconds" {
+  description = "(Optional) Seconds to ignore failing load balancer health checks on newly instantiated tasks to prevent premature shutdown, up to 2147483647. Only valid for services configured to use load balancers."
+  type        = number
+  default     = 0
+}
+
+variable "launch_type" {
+  description = "The launch type on which to run your service. Valid values are `EC2` and `FARGATE`"
+  type        = string
+  default     = "FARGATE"
+}
+
+variable "force_new_deployment" {
+  description = "(Optional) Enable to force a new task deployment of the service. This can be used to update tasks to use a newer Docker image with same image/tag combination (e.g. myimage:latest), roll Fargate tasks onto a newer platform version, or immediately deploy ordered_placement_strategy and placement_constraints updates."
+  type        = bool
+  default     = false
+}
+
+variable "platform_version" {
+  description = "The platform version on which to run your service.Only applicable for launch_type set to FARGATE."
+  type        = string
+  default     = "LATEST"
+}
+
+variable "scheduling_strategy" {
+  description = "The scheduling strategy to use for the service.The valid values are REPLICA and DAEMON.Fargate tasks do not support the DAEMON scheduling strategy."
+  type        = string
+  default     = "REPLICA"
+}
+
+variable "propagate_tags" {
+  description = "(Optional) Specifies whether to propagate the tags from the task definition or the service to the tasks. The valid values are SERVICE and TASK_DEFINITION. Default to SERVICE"
+  default     = "SERVICE"
+}
+
+variable "capacity_provider_strategies" {
+  description = "The capacity provider strategies to use for the service."
+  type = list(object({
+    capacity_provider = string
+    weight            = number
+    base              = number
+  }))
+  default = []
+}
+
+variable "service_registries" {
+  description = "(Optional) The service discovery registries for the service. The maximum number of service_registries blocks is 1. This is a map that should contain the following fields \"registry_arn\", \"port\", \"container_port\" and \"container_name\""
+  type = list(object({
+    registry_arn   = string
+    port           = number
+    container_port = number
+    container_name = string
+  }))
+  default = []
+}
+
+variable "ordered_placement_strategy" {
+  description = "(Optional) Service level strategy rules that are taken into consideration during task placement. List from top to bottom in order of precedence. The maximum number of ordered_placement_strategy blocks is 5. This is a list of maps where each map should contain \"id\" and \"field\""
+  type = list(object({
+    type  = string
+    field = string
+  }))
+  default = []
+}
+
+variable "placement_constraints" {
+  type = list(object({
+    type       = string
+    expression = string
+  }))
+  description = "(Optional) rules that are taken into consideration during task placement. Maximum number of placement_constraints is 10. This is a list of maps, where each map should contain \"type\" and \"expression\""
+  default     = []
+}
+
+variable "ecs_load_balancers" {
+  description = "A list of load balancer config objects for the ECS service."
+  type = list(object({
+    container_name   = string
+    container_port   = number
+    elb_name         = string
+    target_group_arn = string
+  }))
+  default = []
+}
+
+variable "deployment_controller" {
+  description = " (Optional) Type of deployment controller. Valid values: CODE_DEPLOY, ECS, EXTERNAL. Default: ECS."
+  type = list(object({
+    type = string
+  }))
+  default = ["ECS"]
+}
+
+variable "assign_public_ip" {
+  description = "(Optional) Assign a public IP address to the ENI (Fargate launch type only). If true service will be associated with public subnets. Default false. "
+  type        = bool
+  default     = false
+}
+
+variable "security_groups" {
+  description = "(Optional) The security groups associated with the task or service. If you do not specify a security group, the default security group for the VPC is used."
+  type        = list(any)
+  default     = []
+}
+
+variable "public_subnets" {
+  description = "The public subnets associated with the task or service."
+  type        = list(any)
+}
+
+variable "private_subnets" {
+  description = "The private subnets associated with the task or service."
+  type        = list(any)
+}
